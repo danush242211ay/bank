37a3d3d32467d47ae832315a1e86621d40bf2351
danush242211ay <danush8747@gmail.com>
2026-07-06 14:24:05 +0530

    account-model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const authrouter = require('./routes/auth.routes')
 const cookieparser = require('cookie-parser')
 
 
@@ -9,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieparser());
 
-app.use('/api/auth', authrouter)
+const authRouter = require('./routes/auth.routes')
+const accountRouter= require('./routes/account.routes')
+
+app.use('/api/auth', authRouter)
+app.use('/api/accounts',accountRouter)
 
 module.exports = app;
