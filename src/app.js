d8bc5d84a30077a4d65548bc27feb67ce9f304d5
danush242211ay <danush8747@gmail.com>
2026-07-07 12:11:15 +0530

    postman

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cookieparser = require('cookie-parser')
 
-
-
-
+/* middlewares */
 const app = express();
 app.use(express.json());
 app.use(cookieparser());
 
+/* import the routers */
 const authRouter = require('./routes/auth.routes')
 const accountRouter= require('./routes/account.routes')
 
+/* routers use */
 app.use('/api/auth', authRouter)
 app.use('/api/accounts',accountRouter)
 
